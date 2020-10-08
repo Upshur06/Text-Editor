bb66a42b077554cb13969db4e9ed658b869be5a2
Upshur06 <mupshur2005@gmail.com>
2020-10-08 19:07:49 -0400

    ES6 clean up

--- script.js
+++ script.js
@@ -9,12 +9,12 @@
  * Update the output text as a user types in the textarea
  * HINT: Use the onkeydown function inside HTML
  */
-function updateText() {
+updateText = () => {
   // CODE GOES HERE
 
   let text = document.getElementById("text-input").value;
   document.getElementById("text-output").innerText = text;
-}
+};
 
 /**
  * Toggle the bold class for the output text
@@ -23,23 +23,22 @@ function updateText() {
  * HINT: Use the classList property
  * HINT: Toggle .active class for the button
  */
-function makeBold(elem) {
+makeBold = (elem) => {
   //CODE GOES HERE
 
-  // let para = document.getElementsByTagName("para");
   elem.classList.add("active");
   document.getElementById("text-output").classList.toggle("bold");
-  console.log(elem);
-}
+  // console.log(elem);
+};
 
 /**
  * Toggle the italic class for the output text
  */
-function makeItalic(elem) {
+makeItalic = (elem) => {
   elem.classList.add("active");
   document.getElementById("text-output").classList.toggle("italic");
-  console.log(elem);
-}
+  // console.log(elem);
+};
 
 /**
  * Toggle the underline class for the output text
@@ -47,18 +46,15 @@ function makeItalic(elem) {
  * HINT: Use the classList property
  * HINT: Use contains, remove, and add functions
  */
-function makeUnderline(elem) {
+makeUnderline = (elem) => {
   //CODE GOES HERE
   elem.classList.add("active");
   let formattedText = document.getElementById("text-output");
 
-  if (formattedText.classList.contains("underline")) {
-    document.getElementById("text-output").classList.remove("underline");
-  } else {
-    document.getElementById("text-output").classList.add("underline");
-  }
-  // console.log(elem);
-}
+  formattedText.classList.contains("underline")
+    ? document.getElementById("text-output").classList.remove("underline")
+    : document.getElementById("text-output").classList.add("underline");
+};
 
 /**
  * Toggle the style textAlign attribute
@@ -66,36 +62,36 @@ function makeUnderline(elem) {
  * HINT: Use the style property of the element
  * HINT: Make sure to untoggle the active state for all other align buttons
  */
-function alignText(elem, alignType) {
+alignText = (elem, alignType) => {
   // CODE GOES HERE
   document.getElementById("text-output").style.textAlign = alignType;
   let buttonsList = document.getElementsByClassName("align");
 
-  for (let i = 0; i <= buttonsList.length - 1; i++) {
-    buttonsList[i].classList.remove("active");
+  for (let button of buttonsList) {
+    button.classList.remove("active");
   }
   elem.classList.add("active");
-}
+};
 
-function clear() {
+clear = () => {
   // document.getElementById("text-input").value = "";
   // document.getElementById("text-output").value = "";
 
-  document.getElementById("wipeOut").addEventListener("click", function () {
+  document.getElementById("wipeOut").addEventListener("click", () => {
     document.getElementById("text-input").value = "";
     document.getElementById("text-output").innerText = "";
     warning();
   });
-}
+};
 
-function warning() {
-  setTimeout(function () {
+warning = () => {
+  setTimeout(() => {
     document.getElementsByClassName("alert")[0].style.display = "block";
   }, 600);
 
-  setTimeout(function () {
+  setTimeout(() => {
     document.getElementsByClassName("alert")[0].style.display = "none";
   }, 5000);
-}
+};
 
 clear();
